253b5f26f78fc6cfedbffb20fbcf8393eb9ba3e6
eliel <silvaeliel940@gmail.com>
2022-08-25 23:55:31 -0300

    feat: created model in the file src/database/models/category.js

---
+++ src/database/models/category.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: { 
+      type: DataTypes.INTEGER, 
+      primaryKey: true, 
+      autoIncrement: true 
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    timestamps: false, // remove a obrigatoriedade de utilizar os campos `createdAt` e `updatedAt`
+    tableName: 'Category',
+  });
+
+  return Category;
+};
